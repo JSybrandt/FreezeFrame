07c056e5d6743056a866c20079e9a3cc43e2006d
Darkfire16 <gmodman12@gmail.com>
2014-11-03 09:21:15 -0500

    moved around checks for going out of bounds for timeMultiplier, and made it so the player speed is tied to frametime.

--- FreezeFrame/FreezeFrame/Player.cpp
+++ FreezeFrame/FreezeFrame/Player.cpp
@@ -49,22 +49,34 @@ void Player::update(float &frametime)
 
 		D3DXVec2Normalize(&inputDir,&inputDir);
 
-		inputDir *= playerNS::SPEED*frametime;
+		//inputDir *= playerNS::SPEED*frametime;				//TESTING
 
-		setCenter(getCenter()+inputDir);
+		inputDir *= playerNS::SPEED;							
 
 		if(inputDir == VECTOR2(0,0) && timeMultiplier > 0.01 ) { //
 			timeMultiplier -= 0.05;
-			frametime *= timeMultiplier;
 			if(timeMultiplier < 0.01)
 				timeMultiplier = 0.01;
+			frametime *= timeMultiplier;
+			
 			//frametime *= .01;				//If need this back, just comment out anything with time multiplier.
 		}
+
+		else if(inputDir == VECTOR2(0,0))
+			frametime *= timeMultiplier;
+
 		else if(timeMultiplier < 1) {
 			timeMultiplier += 0.05;
+			if(timeMultiplier > 1)
+				timeMultiplier = 1;
 			frametime *= timeMultiplier;
 		}
-		
+
+		inputDir *= frametime;					
+
+		setCenter(getCenter()+inputDir);
+
+		//ifs go here normally
 
 		weaponCooldown -= frametime;
 		if(weaponCooldown < 0) weaponCooldown =0;
